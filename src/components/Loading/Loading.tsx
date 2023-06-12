e0e0e6f300b75c767283a9387703f808688bce15
kevindm14 <kevindiazm.14@gmail.com>
2023-06-12 01:23:21 -0500

    fix: modifying the loading spinner

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,7 @@
 export const Loading = () => {
     return (
-        <div className="flex justify-center items-center">
-            <div className="spinner"></div>
+        <div className="flex justify-center items-center h-screen bg-blue opacity-30">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue"></div>
         </div>
     )
 }
