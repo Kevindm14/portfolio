119ad95e50f6926f15516edf7bae2cbd8d0bf305
kevindm14 <kevindiazm.14@gmail.com>
2024-04-18 18:42:28 -0500

    feat: adding animation to icons

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -12,21 +12,25 @@ export const Hero = () => {
     >
       <Container className="flex flex-col items-center lg:flex-row lg:justify-between">
         <div className="mb-32 lg:mb-0 relative">
-          <IconsLanguages.Golang className="hidden lg:block absolute -top-32 w-16 h-16 lg:w-20 lg:h-20 text-gray" />
-          <IconsLanguages.Ruby className="hidden lg:block absolute left-64 -top-28 right w-16 h-16 lg:w-20 lg:h-20 text-gray" />
-          <IconsJS.ReactJS className="hidden lg:block absolute -bottom-40 w-16 h-16 lg:w-20 lg:h-20 text-gray" />
-          <IconsJS.NodeJS className="hidden lg:block absolute left-64 -bottom-36 right w-16 h-16 lg:w-20 lg:h-20 text-gray" />
-          <IconsJS.JS className="hidden lg:block absolute left-96 top-20 w-16 h-16 lg:w-20 lg:h-20 text-gray" />
+          <IconsLanguages.Golang className="filter-neon hidden lg:block absolute -top-32 w-16 h-16 lg:w-20 lg:h-20 text-gray" />
+          <IconsLanguages.Ruby className="filter-neon hidden lg:block absolute left-64 -top-28 right w-16 h-16 lg:w-20 lg:h-20 text-gray" />
+          <IconsJS.ReactJS className="filter-neon hidden lg:block absolute -bottom-40 w-16 h-16 lg:w-20 lg:h-20 text-gray" />
+          <IconsJS.NodeJS className="filter-neon hidden lg:block absolute left-64 -bottom-36 right w-16 h-16 lg:w-20 lg:h-20 text-gray" />
+          <IconsJS.JS className="filter-neon hidden lg:block absolute left-96 top-20 w-16 h-16 lg:w-20 lg:h-20 text-gray" />
 
           <h1 className="text-zaffre shadow mx-auto font-extrabold lg:text-2xl">Hola, Soy</h1>
           <h2 className="text-white lg:mb-4 mx-auto font-extrabold text-6xl">
             Kevin <span className="text-zaffre stroke-white shadow">Diaz</span>
           </h2>
-          <h3 className="text-2xl lg:text-3xl font-extrabold">Un Desarrollador Web</h3>
+          <h3 className="text-2xl lg:text-3xl font-extrabold mb-5">Un Desarrollador Web</h3>
 
-          <button className="border-2 w-72 mt-5 lg:mt-5 font-medium py-2 rounded-lg hover:text-neutrals-900 hover:border-white hover:bg-white transition ease-in">
+          <a
+            href='https://drive.google.com/file/d/1C4sGHiftuzKAcWuAz-Lk7aU4CQ8wz-ph/view'
+            target='_blank'
+            className="border-2 w-72 font-bold py-2 px-8 rounded-md hover:text-neutrals-900 hover:border-white hover:bg-white transition ease-in"
+          >
             Mi Curriculum
-          </button>
+          </a>
         </div>
 
         <div className="relative w-[200px] md:w-[300px] xl:w-[400px] lg:mt-0">
